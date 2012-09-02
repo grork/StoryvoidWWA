6ba9dd3c14dedd59609bffed4ecf21e201d2c34b
Dominic Hopton <d.hopton@codevoid.net>
2012-09-02 20:20:28 +0000

    - Factored shared test helpers for instapaper tests into new file - Added new file for sync client - Added synd client tests
    Work in Progress.

---
+++ ArticleVoid/js/InstapaperSync.js
@@ -0,0 +1,14 @@
+﻿(function () {
+    "use strict";
+
+    WinJS.Namespace.define("Codevoid.ArticleVoid", {
+        InstapaperSync: WinJS.class.define(function() {
+        }, {
+            _folders: null,
+            _bookmarks: null,
+            _db: null,
+            sync: function sync() {
+            },
+        }),
+    });
+});
---
+++ ArticleVoid/js/tests/InstapaperTests_Utilities.js
@@ -0,0 +1,78 @@
+﻿(function () {
+    "use strict";
+    
+    var InstapaperDB = Codevoid.ArticleVoid.InstapaperDB;
+    var pendingDbs = [];
+    
+    function cleanUpOpenDbs() {
+        pendingDbs.forEach(function (idb) {
+            idb.dispose();
+        });
+
+        pendingDbs = [];
+    }
+
+    function getNewInstapaperDBAndInit() {
+        return new InstapaperDB().initialize().then(function (idb) {
+            pendingDbs.push(idb);
+
+            return idb;
+        });
+    }
+
+    function startOnSuccessOfPromise() {
+        cleanUpOpenDbs();
+        start();
+    }
+
+    function startOnFailureOfPromise(error) {
+        debugger;
+        ok(false, "Failed: " + error.toString() + "\n" + error.stack);
+        cleanUpOpenDbs();
+        start();
+    }
+
+    function promiseTest(name, func) {
+        asyncTest(name, function () {
+            WinJS.Promise.as(func()).done(startOnSuccessOfPromise, startOnFailureOfPromise);
+        });
+    }
+
+    function expectNoPendingFolderEdits(idb) {
+        return idb.getPendingFolderEdits().then(function (pendingEdits) {
+            ok(pendingEdits, "Expected valid pending edits structure");
+            strictEqual(pendingEdits.length, 0, "Didn't expect to find any pending edits");
+        });
+    }
+
+    function expectNoPendingBookmarkEdits(idb) {
+        return idb.getPendingBookmarkEdits().then(function (pendingEdits) {
+            ok(pendingEdits, "Expected valid pending edits structure");
+            strictEqual(pendingEdits.length, 0, "Didn't expect to find any pending edits");
+        });
+    }
+
+    function deleteDb() {
+        pendingDbs.forEach(function (idb) {
+            idb.dispose();
+        });
+
+        pendingDbs = [];
+
+        return WinJS.Promise.timeout().then(function () {
+            return db.deleteDb(InstapaperDB.DBName);
+        }).then(function () {
+            ok(true);
+        });
+    }
+
+    WinJS.Namespace.define("InstapaperTestUtilities", {
+        getNewInstapaperDBAndInit: getNewInstapaperDBAndInit,
+        startOnSuccessOfPromise: startOnSuccessOfPromise,
+        startOnFailureOfPromise: startOnFailureOfPromise,
+        promiseTest: promiseTest,
+        expectNoPendingFolderEdits: expectNoPendingFolderEdits,
+        expectNoPendingBookmarkEdits: expectNoPendingBookmarkEdits,
+        deleteDb: deleteDb,
+    });
+})();
