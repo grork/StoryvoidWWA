3ee7dc556589ab85cc03fc710b69b0d88725a0ef
Dominic Hopton <d.hopton@codevoid.net>
2012-12-27 20:28:42 +0000

    Ported control et al over to typescript

--- ArticleVoid/js/ui/core.ts
+++ ArticleVoid/js/ui/core.ts
@@ -1,28 +1,26 @@
 ﻿/// <reference path="..\..\tsc\libs\winjs.d.ts" static="true" />
 /// <reference path="..\..\tsc\libs\codevoid.d.ts" static="true" />
-(function () {
-    "use strict";
 
-    WinJS.Namespace.define("Codevoid.UICore", {
-        Control: WinJS.Class.define(function (element, options) {
+module Codevoid.UICore {
+    export class Control {
+        element: HTMLElement;
+        constructor(element: HTMLElement, options: any) {
             this.element = element;
             WinJS.UI.setOptions(this, options);
-        }, {
-            element: null,
-        }),
-        currentViewType: "unittest",
-        getViewForModel: function (model) {
-            if (!model.view) {
-                throw new Error("Can't get a view for a model that doesn't have a view");
-            }
+        }
+    }
+    export var currentViewType: string = "unittest";
+    export function getViewForModel(model: any): any {
+        if (!model.view) {
+            throw new Error("Can't get a view for a model that doesn't have a view");
+        }
 
-            var view = model.view[Codevoid.UICore.currentViewType];
-            if (!view) {
-                throw new Error("Can't get view for the current view type");
-            }
+        var view: string = model.view[Codevoid.UICore.currentViewType];
+        if (!view) {
+            throw new Error("Can't get view for current view type");
+        }
 
-            var ctor = WinJS.Utilities.getMember(view);
-            return ctor;
-        },
-    });
-})();
+        var ctor = WinJS.Utilities.getMember(view);
+        return ctor;
+    }
+}
