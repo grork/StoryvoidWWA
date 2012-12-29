a54c66219352137c7dfbcdbc311780870c713d91
Dominic Hopton <d.hopton@codevoid.net>
2012-12-29 01:29:11 +0000

    Adding experience manager, and tests

---
+++ ArticleVoid/tsc/libs/qunit.d.ts
@@ -0,0 +1,17 @@
+﻿declare function test(testName: string, testFunction: () => void );
+declare function asyncTest(testName: string, testFunction: () => void );
+declare function module(moduleName: string);
+declare function start();
+declare function stop();
+declare function ok(result: any, message: string);
+declare function equal(actual: any, expected: any, message: string);
+declare function notEqual(actual: any, expected: any, message: string);
+declare function deepEqual(actual: any, expected: any, message: string);
+declare function notDeepEqual(actual: any, expected: any, message: string);
+declare function strictEqual(actual: any, expected: any, message: string);
+declare function notStrictEqual(actual: any, expected: any, message: string);
+declare var QUnit: QUnit;
+
+interface QUnit {
+    module(moduleName: string);
+}
