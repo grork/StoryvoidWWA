9aae7961df455fa6779fd09416603439d1d302a9
Dominic Hopton <d.hopton@codevoid.net>
2015-10-05 01:12:29 +0000

    Adding Basic App into project * App Project, Manifest etc etc * Copied over the min winjs from test project * Removed "js" and made app object start from TS * Added margin, empty do-nothing label * Logs "done" when it's completely loaded

---
+++ App/js/default.ts
@@ -0,0 +1,17 @@
+﻿/// <reference path="../scripts/typings/winjs/winjs.d.ts" />
+
+module Codevoid.ArticleVoid {
+    export class App {
+        constructor() {
+        }
+
+        public initialize(): void {
+            WinJS.Utilities.ready().done(() => {
+                console.log("Done");
+            });
+        }
+    }
+
+    var app = (<any>window).__appObject = new App();
+    app.initialize();
+}
