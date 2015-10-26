abf5eebc4e5b391ddd94f0ba440b257b1704ce45
Dominic Hopton <d.hopton@codevoid.net>
2015-10-26 04:22:43 +0000

    Added support for signed in state changes to the main app experience - now you can sign in & the UI loads the "signed in" experience, as well as the "sign out" to return you to logged out state.
    Also, added spinner to the sign in dialog, because it was annoying not to see if it was working

--- App/js/default.ts
+++ App/js/default.ts
@@ -1,23 +1,55 @@
 ﻿module Codevoid.ArticleVoid {
     export class App {
+        private _signedOutViewModel: Codevoid.ArticleVoid.UI.SignedOutViewModel;
+        private _signedInViewModel: Codevoid.ArticleVoid.UI.SignedInViewModel;
         constructor() {
+            if (App._instance) {
+                throw "Only one app instance can be constructed";
+            }
+
+            App._instance = this;
         }
 
         public initialize(): void {
             Codevoid.UICore.Experiences.initializeHost(new Codevoid.UICore.WwaExperienceHost(document.body));
-            var signedInElement = <HTMLElement>document.body.firstElementChild;
 
             if (!Codevoid.ArticleVoid.Authenticator.hasStoredCredentials()) {
-                WinJS.Utilities.addClass(signedInElement, "hide");
-                Codevoid.UICore.Experiences.currentHost.addExperienceForModel(new Codevoid.ArticleVoid.UI.SignedOutViewModel());
+                this.signedOut();
             } else {
-                (<Codevoid.UICore.WwaExperienceHost>Codevoid.UICore.Experiences.currentHost).createExperienceWithModel(signedInElement, new Codevoid.ArticleVoid.UI.SignedInViewModel());
+                this.signedIn();
             }
+        }
+
+        public signedOut(): void {
+            var signedInElement = <HTMLElement>document.body.firstElementChild;
+            WinJS.Utilities.addClass(signedInElement, "hide");
+            this._signedOutViewModel = new Codevoid.ArticleVoid.UI.SignedOutViewModel();
+            Codevoid.UICore.Experiences.currentHost.addExperienceForModel(this._signedOutViewModel);
+        }
+
+        public signedIn(): void {
+            var signedInElement = <HTMLElement>document.body.firstElementChild;
+            if (!this._signedInViewModel) {
+                this._signedInViewModel = new Codevoid.ArticleVoid.UI.SignedInViewModel();
+                (<Codevoid.UICore.WwaExperienceHost>Codevoid.UICore.Experiences.currentHost).createExperienceWithModel(signedInElement, this._signedInViewModel);
+            }
+
+            WinJS.Utilities.removeClass(signedInElement, "hide");
+
+            if (this._signedOutViewModel) {
+                Codevoid.UICore.Experiences.currentHost.removeExperienceForModel(this._signedOutViewModel);
+                this._signedOutViewModel = null;
+            }
+        }
+
+        static _instance: App;
+        static get instance(): App {
+            return App._instance;
         }
     }
 
     WinJS.Utilities.ready().done(() => {
-        var app = (<any>window).__appObject = new App();
+        var app = new App();
         app.initialize();
     });
 }
