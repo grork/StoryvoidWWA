97588e5320dcee290f353dbab32537326830d41e
Dominic Hopton <d.hopton@codevoid.net>
2015-12-13 05:26:18 +0000

    Basic Share contract. Handles sign in/signout, but doesn't actually do anything with the data.

---
+++ App/js/shareTarget.ts
@@ -0,0 +1,55 @@
+﻿module Codevoid.ArticleVoid {
+    export class ShareTargetApp extends UI.AppThatCanSignIn {
+
+        public initialize(): void {
+            super.initialize();
+
+            Windows.UI.WebUI.WebUIApplication.addEventListener("activated", (args) => {
+                var shareArgs = <Windows.ApplicationModel.Activation.IShareTargetActivatedEventArgs>args;
+
+                Utilities.Logging.instance.log("From: " + shareArgs.shareOperation.data.properties.title);
+
+                shareArgs.shareOperation.data.getUriAsync().done((uri: Windows.Foundation.Uri) => {
+                    Utilities.Logging.instance.log(uri.rawUri);
+                });
+            });
+        }
+
+        protected getSignedInViewModel(): UI.ISignedInViewModel {
+            return new UI.ShareTargetSignedInViewModel(this);
+        }
+    }
+
+    WinJS.Utilities.ready().done(() => {
+        var app = new ShareTargetApp();
+        app.initialize();
+
+        Utilities.Logging.instance.showViewer();
+    });
+}
+
+module Codevoid.ArticleVoid.UI {
+    export class ShareTargetSignedInViewModel implements ISignedInViewModel {
+        public experience = { wwa: "Codevoid.ArticleVoid.UI.ShareTargetSignedInExperience" };
+        private _app: ShareTargetApp;
+        private _clientInformation: Codevoid.OAuth.ClientInformation;
+        private _eventSource: Utilities.EventSource;
+
+        constructor(app: IAppWithAbilityToSignIn) {
+            this._app = <ShareTargetApp>app;
+            this._eventSource = new Utilities.EventSource();
+        }
+
+        public signedIn(): void {
+        }
+    }
+
+    export class ShareTargetSignedInExperience extends UICore.Control {
+        constructor(element: HTMLElement, options: any) {
+            super(element, options);
+
+            Utilities.DOM.setControlAttribute(element, "Codevoid.ArticleVoid.UI.ShareTargetSignedInExperience");
+            element.textContent = "Signed in!";
+        }
+    }
+}
