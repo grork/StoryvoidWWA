d965ef5454ef55249e102d9ea881764f1f64eede
Dominic Hopton <d.hopton@codevoid.net>
2016-01-01 02:21:01 +0000

    Adding "Setup" step to the tests for Article sync tests: * create articles on service in unread folder * clean all local state * sync only unread folder

--- ArticleVoidTest/js/tests/InstapaperArticleSync_tests.ts
+++ ArticleVoidTest/js/tests/InstapaperArticleSync_tests.ts
@@ -1,9 +1,73 @@
 ﻿module CodevoidTests.InstapaperArticleSyncTests {
+    import st = Windows.Storage;
+    import av = Codevoid.ArticleVoid;
+
+    var promiseTest = InstapaperTestUtilities.promiseTest;
+    var startOnSuccessOfPromise = InstapaperTestUtilities.startOnSuccessOfPromise;
+    var startOnFailureOfPromise = InstapaperTestUtilities.startOnFailureOfPromise;
+
+    var clientID = "Uzf6U3vHqc7vcMUKSj7JpYvungTSjQVEoyfyJtYtHdX6wWQ05J";
+    var clientSecret = "z4KurzIZ21NFJgFopHRqObIjNEHe5uFECBzpjQ809oFNbxi0lm";
+
+    var token = "ildNcJmVDn4O5F5Z2V5X8TSNc1pC1aqY98pCOYObAmoc4lGQSD";
+    var secret = "gcl8m34CfruNsYEKuRCdvClxqMOC5rxiTpXfrThV6sCgwMktsf";
+
+    var clientInformation = new Codevoid.OAuth.ClientInformation(clientID, clientSecret, token, secret);
+    clientInformation.productName = "Codevoid InstapaperArticleSync Tests";
+
+    // Sample, known articles
+    var normalArticleUrl = "http://www.codevoid.net/articlevoidtest/TestPage1.html";
+    var normalArticleId: number;
+    var articleWithImageUrl = "http://www.codevoid.net/articlevoidtest/TestPage9.html";
+    var articleWithImageId: number;
+
+    function deleteAllLocalFiles(): WinJS.Promise<any> {
+        return st.ApplicationData.current.temporaryFolder.createFolderAsync("ArticleTemp", st.CreationCollisionOption.openIfExists).then((folder: st.StorageFolder) => {
+            return folder.getFilesAsync();
+        }).then((files: Windows.Foundation.Collections.IVectorView<st.StorageFile>) => {
+            var deletes = files.map((file: st.StorageFile) => {
+                return file.deleteAsync();
+            });
+
+            return WinJS.Promise.join(deletes);
+        });
+    }
 
     QUnit.module("InstapaperArticleSyncTests");
 
     test("canInstantiateArticleSync", () => {
-        var syncEngine = new Codevoid.ArticleVoid.InstapaperArticleSync();
+        var syncEngine = new av.InstapaperArticleSync(clientInformation);
         notStrictEqual(syncEngine, null, "Should have constructed new article sync engine");
+    });
+
+    // Remove remote data for known state
+    promiseTest("setupLocalAndRemoteState", () => {
+        var dbInstance = new av.InstapaperDB();
+
+        var api = new av.InstapaperApi.Bookmarks(clientInformation);
+        return WinJS.Promise.join([
+            api.add({ url: normalArticleUrl }).then((article: av.IBookmark) => {
+                normalArticleId = article.bookmark_id;
+            }),
+            api.add({ url: articleWithImageUrl }).then((article: av.IBookmark) => {
+                articleWithImageId = article.bookmark_id;
+            }),
+            deleteAllLocalFiles().then(() => { // Clear any downloaded files
+                return InstapaperTestUtilities.deleteDb(); // Clear the DB itself
+            }),
+        ]).then(() => {
+            return dbInstance.initialize();
+        }).then(() => {
+            var sync = new av.InstapaperSync(clientInformation);
+            return sync.sync({
+                dbInstance: dbInstance,
+                folders: false,
+                bookmarks: true,
+                singleFolder: true,
+                folder: dbInstance.commonFolderDbIds.unread
+            });
+        }).then(() => {
+            dbInstance.dispose();
+        });
     });
 }
--- ArticleVoidTest/tsc/libs/codevoidtest.d.ts
+++ ArticleVoidTest/tsc/libs/codevoidtest.d.ts
@@ -1,5 +1,9 @@
 ﻿declare module InstapaperTestUtilities {
-    export function promiseTest(name: string, func: () => WinJS.Promise<any> , delay?: number);
+    export function promiseTest(name: string, func: () => WinJS.Promise<any>, delay?: number);
+    export function startOnFailureOfPromise();
+    export function startOnSuccessOfPromise();
+    export function destroyRemoteData(clientInformation: Codevoid.OAuth.ClientInformation): WinJS.Promise<any>;
+    export function deleteDb(): WinJS.Promise<any>;
     export function getPlayground(): HTMLElement;
 }
 
