d59ebd0b3b91998ae9578dd9ba1560a5b42a4cfb
Dominic Hopton <d.hopton@codevoid.net>
2016-02-11 07:18:29 +0000

    Added Layout to Article Viewer * Animation on show/hide * Added 'x' dismiss button * Added title * Handle escape to dismiss

--- App/js/ui/ArticleViewerExperience.ts
+++ App/js/ui/ArticleViewerExperience.ts
@@ -11,18 +11,36 @@
             WinJS.Utilities.addClass(element, "articleViewer-container");
             WinJS.Utilities.addClass(element, "dialog");
             WinJS.Utilities.addClass(element, "win-disposable");
+            WinJS.Utilities.addClass(element, "hide");
 
             DOM.loadTemplate("/HtmlTemplates.html", "articleViewer").then((template) => {
-                return template.render(null, element);
+                return template.render(this.viewModel, element);
             }).done(() => {
                 DOM.setControlAttribute(element, "Codevoid.ArticleVoid.UI.SignedOutExperience");
                 this._handlersToCleanup.push(DOM.marryEventsToHandlers(element, this));
                 DOM.marryPartsToControl(element, this);
+
+                this._handlersToCleanup.push(Codevoid.Utilities.addEventListeners(window, {
+                    keyup: (e: KeyboardEvent) => {
+                        if (e.keyCode != WinJS.Utilities.Key.escape) {
+                            return;
+                        }
+
+                        this.close();
+                    }
+                }));
+
+                WinJS.Promise.timeout().done(() => {
+                    WinJS.Utilities.removeClass(element, "hide");
+                    WinJS.UI.Animation.slideUp(this.element);
+                });
             });
         }
 
         public close(): void {
-            Codevoid.UICore.Experiences.currentHost.removeExperienceForModel(this.viewModel);
+            WinJS.UI.Animation.slideDown(this.element).done(() => {
+                Codevoid.UICore.Experiences.currentHost.removeExperienceForModel(this.viewModel);
+            });
         }
 
         public dispose(): void {
@@ -36,7 +54,7 @@
 
     export class ArticleViewerViewModel implements Codevoid.UICore.ViewModel {
         public experience = { wwa: "Codevoid.ArticleVoid.UI.ArticleViewerExperience" };
-        constructor() {
+        constructor(public bookmark: IBookmark) {
         }
     }
 
