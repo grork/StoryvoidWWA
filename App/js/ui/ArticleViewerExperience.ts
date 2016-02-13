1caf2cd7ad9485a3fd2ef8a846d1f462e0b1c2c0
Dominic Hopton <d.hopton@codevoid.net>
2016-02-13 03:32:33 +0000

    Update Article viewer to change the titlebar colour when viewed

--- App/js/ui/ArticleViewerExperience.ts
+++ App/js/ui/ArticleViewerExperience.ts
@@ -4,6 +4,8 @@
     export class ArticleViewerExperience extends Codevoid.UICore.Control {
         private _handlersToCleanup: Codevoid.Utilities.ICancellable[] = [];
         private viewModel: ArticleViewerViewModel;
+        private _previousPrimaryColour: Windows.UI.Color;
+        private _previousTextColour: Windows.UI.Color;
 
         constructor(element: HTMLElement, options: any) {
             super(element, options);
@@ -32,12 +34,38 @@
 
                 WinJS.Promise.timeout().done(() => {
                     WinJS.Utilities.removeClass(element, "hide");
+                    this._setTitleBarForArticle();
                     WinJS.UI.Animation.slideUp(this.element);
                 });
             });
         }
 
+        private _setTitleBarForArticle(): void {
+            var titleBar = Windows.UI.ViewManagement.ApplicationView.getForCurrentView().titleBar;
+
+            this._previousPrimaryColour = titleBar.backgroundColor;
+            this._previousTextColour = titleBar.foregroundColor;
+
+            this._setTitleBar(Windows.UI.Colors.white, Windows.UI.Colors.gray);
+        }
+
+        private _restoreTitlebar(): void {
+            this._setTitleBar(this._previousPrimaryColour, this._previousTextColour);
+        }
+
+        private _setTitleBar(primaryColour: Windows.UI.Color, textColour: Windows.UI.Color): void {
+            var titleBar = Windows.UI.ViewManagement.ApplicationView.getForCurrentView().titleBar;
+
+            titleBar.backgroundColor = primaryColour;
+            titleBar.buttonBackgroundColor = primaryColour;
+            titleBar.buttonForegroundColor = textColour;
+            titleBar.foregroundColor = textColour;
+            titleBar.inactiveBackgroundColor = primaryColour;
+            titleBar.buttonInactiveBackgroundColor = primaryColour;
+        }
+
         public close(): void {
+            this._restoreTitlebar();
             WinJS.UI.Animation.slideDown(this.element).done(() => {
                 Codevoid.UICore.Experiences.currentHost.removeExperienceForModel(this.viewModel);
             });
