41757c30cfcb1fe110f79a1946f83b6eed445c80
Dominic Hopton <d.hopton@codevoid.net>
2016-02-17 04:09:35 +0000

    Add round trip messaging to web messenger. Includes tests.

--- ArticleVoidTest/js/WebViewMessenger_client.ts
+++ ArticleVoidTest/js/WebViewMessenger_client.ts
@@ -1,6 +1,81 @@
 ﻿module Codevoid.Utilities {
-    export class WebViewMessenger_Client {
+    interface IMessageDetails {
+        message: string;
+        responseId: number;
+        payload: any;
     }
 
-    (<any>window.external).notify(JSON.stringify({ message: "ready" }));
+    export class WebViewMessenger_Client {
+        private _messageHandlers: { [id: string]: (payload: any, completion: (payload: any) => void) => any } = {};
+
+        constructor() {
+        }
+
+        private _handleMessage(args): void {
+            var data: IMessageDetails = JSON.parse(args);
+
+            switch (data.message.toLowerCase()) {
+                case "ping":
+                    this.sendMessage("response", "pong", data.responseId);
+                    break;
+
+                case "addscript":
+                    this._addScript(data.payload.url, data.responseId);
+                    break;
+
+                default:
+                    var handler = this._messageHandlers[data.message];
+                    if (handler) {
+                        handler(data.payload, this._getCompletionForId(data.responseId));
+                    }
+                    break;
+            }
+        }
+
+        private _getCompletionForId(responseId: number): (payload: any) => void {
+            return (payload: any) => {
+                this.sendMessage("response", payload, responseId);
+            };
+        }
+
+        private _addScript(scriptPath: string, responseId: number): void {
+            var scriptTag = document.createElement("script");
+            scriptTag.addEventListener("load", () => {
+                this.sendMessage("response", null, responseId);
+            });
+
+            scriptTag.src = scriptPath;
+
+            document.head.appendChild(scriptTag);
+        }
+
+        public initialize(): void {
+            document.addEventListener("DOMContentLoaded", () => {
+                this.sendMessage("ready", null);
+            });
+
+            (<any>window).messageHandler = this._handleMessage.bind(this);
+        }
+
+        public sendMessage(message: string, payload: any, responseId?: number): void {
+            var notifyData = {
+                message: message,
+                payload: payload,
+                responseId: responseId
+            };
+
+            (<any>window.external).notify(JSON.stringify(notifyData));
+        }
+
+        public addHandlerForMessage(message: string, handler: (payload: any, completion: (payload: any) => void) => any) {
+            this._messageHandlers[message] = handler;
+        }
+
+        public static Instance: WebViewMessenger_Client;
+    }
+
+    var viewerClient = new WebViewMessenger_Client();
+    WebViewMessenger_Client.Instance = viewerClient;
+
+    viewerClient.initialize();
 }
--- ArticleVoidTest/js/tests/WebViewMessengerTests.ts
+++ ArticleVoidTest/js/tests/WebViewMessengerTests.ts
@@ -22,4 +22,48 @@
 
         return s.promise;
     });
+
+    promiseTest("webViewCanInvokeMessageRemotely", () => {
+        var s = new Signal();
+
+        var div = getPlayground();
+        var webView = <MSHTMLWebViewElement>div.appendChild(document.createElement("x-ms-webview"));
+        var messenger = new WebViewMessenger(webView);
+        messenger.events.addEventListener("ready", () => {
+            s.complete();
+        });
+
+        webView.navigate("ms-appx-web:///TestWebViewTarget.html");
+
+        ok(!!messenger, "Didn't construct messenger");
+
+        return s.promise.then(() => {
+            return messenger.invokeForResult("ping");
+        }).then((message) => {
+            strictEqual(message, "pong", "Incorrect message");
+        });
+    });
+
+    promiseTest("webViewCanInjectAdditionalScript", () => {
+        var s = new Signal();
+
+        var div = getPlayground();
+        var webView = <MSHTMLWebViewElement>div.appendChild(document.createElement("x-ms-webview"));
+        var messenger = new WebViewMessenger(webView);
+        messenger.events.addEventListener("ready", () => {
+            s.complete();
+        });
+
+        webView.navigate("ms-appx-web:///TestWebViewTarget.html");
+
+        ok(!!messenger, "Didn't construct messenger");
+
+        return s.promise.then(() => {
+            return messenger.addAdditionalScriptInsideWebView("ms-appx-web:///js/tests/WebViewTestScript.js");
+        }).then(() => {
+            return messenger.invokeForResult("gettest");
+        }).then((testCookie: number) => {
+            strictEqual(testCookie, 42, "Got incorrect test cookie");
+        });
+    });
 }
