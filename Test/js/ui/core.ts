55b4a7402d1f67fed3a3d4903720f52d0369e016
Dominic Hopton <d.hopton@codevoid.net>
2016-04-06 04:03:19 +0000

    Undo Change 262 because VS Update 2 is horked for TypeScript

--- Test/js/ui/core.ts
+++ Test/js/ui/core.ts
@@ -88,6 +88,7 @@ module Codevoid.UICore {
             for (var i = 0; i < this.host.children.length; i++) {
                 if (viewModel === (<HTMLExperienceElement>this.host.children[i]).model) {
                     return <HTMLExperienceElement>this.host.children[i];
+                    break;
                 }
             }
 
