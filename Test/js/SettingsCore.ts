eed309c7843a33838cc54ffb7f590d630eaaadc6
Dominic Hopton <d.hopton@codevoid.net>
2016-04-25 04:49:45 +0000

    Add basic settings retrieval * Added wrapper test method to make it easy to ensure things are cleaned up correctly

--- Test/js/SettingsCore.ts
+++ Test/js/SettingsCore.ts
@@ -1,6 +1,39 @@
 ﻿module Codevoid.Utilities {
+    import st = Windows.Storage;
+
     export abstract class SettingsCore {
-        constructor(containerName: string) {
+        private _settingsContainer: st.ApplicationDataContainer;
+
+        constructor(protected _containerName: string,
+                    private _parentContainer: st.ApplicationDataContainer) {
+        }
+
+        private get container(): st.ApplicationDataContainer {
+            if (!this._settingsContainer) {
+                this._settingsContainer = this._parentContainer.createContainer(this._containerName, st.ApplicationDataCreateDisposition.always);
+            }
+
+            return this._settingsContainer;
+        }
+
+        protected getValueOrDefault<T>(name: string, defaultValue: T): T {
+            var settingValue = this.container.values.lookup(name);
+            
+            // If value wasn't actually there, just return the
+            // default value.
+            if (settingValue == null) {
+                return defaultValue;
+            }
+
+            return settingValue;
+        }
+
+        public removeAllSettings(): void {
+            if (!this._settingsContainer) {
+                return;
+            }
+
+            this._parentContainer.deleteContainer(this._settingsContainer.name);
         }
     }
 }
