7ba21b99e3b19e94f6600037044155af6e7571af
Dominic Hopton <d.hopton@codevoid.net>
2016-04-26 05:41:22 +0000

    Adding Skeleton class for viewer settings

---
+++ App/js/ViewerSettings.ts
@@ -0,0 +1,7 @@
+﻿module Codevoid.Storyvoid {
+    export class ViewerSettings extends Codevoid.Utilities.SettingsCore {
+        constructor() {
+            super("ViewerSettings", Windows.Storage.ApplicationData.current.localSettings);
+        }
+    }
+}
