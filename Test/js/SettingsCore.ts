397f622be8bb6c15aeabf4730298c50de5414e0c
Dominic Hopton <d.hopton@codevoid.net>
2016-04-28 05:48:35 +0000

    Refactored settings to better support lazy creation of the container Added unit tests for afformentioned scenario

--- Test/js/SettingsCore.ts
+++ Test/js/SettingsCore.ts
@@ -5,7 +5,12 @@
         private _settingsContainer: st.ApplicationDataContainer;
 
         constructor(protected _containerName: string,
-                    private _parentContainer: st.ApplicationDataContainer) {
+            private _parentContainer: st.ApplicationDataContainer,
+            private _defaults: { [id: string]: any }) {
+        }
+
+        private get containerCreated(): boolean {
+            return this._parentContainer.containers.hasKey(this._containerName);
         }
 
         private get container(): st.ApplicationDataContainer {
@@ -16,19 +21,29 @@
             return this._settingsContainer;
         }
 
-        protected getValueOrDefault<T>(name: string, defaultValue: T): T {
+        protected getValueOrDefault<T>(name: string): T {
+            if (!this.containerCreated) {
+                return this._defaults[name];
+            }
+
             var settingValue = this.container.values.lookup(name);
             
             // If value wasn't actually there, just return the
             // default value.
             if (settingValue == null) {
-                return defaultValue;
+                return this._defaults[name];
             }
 
             return settingValue;
         }
 
         protected setValue<T>(name: string, value: T): void {
+            // Don't create the value if it's the same as the default, and the
+            // container doesn't exist.
+            if (!this.containerCreated && (this._defaults[name] == value)) {
+                return;
+            }
+
             this.container.values.insert(name, value);
         } 
 
