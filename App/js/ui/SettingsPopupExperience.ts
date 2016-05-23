c79717083fcf054d9565f4b015c8c32525c2bf89
Dominic Hopton <d.hopton@codevoid.net>
2016-05-23 01:07:27 +0000

    Improve focus/keyboarding in Settings popup a) Set initial focus b) Handle escape properly by going fucking nuts and handling all the keys on select elements. See code for details.

--- App/js/ui/SettingsPopupExperience.ts
+++ App/js/ui/SettingsPopupExperience.ts
@@ -2,13 +2,17 @@
     import DOM = Codevoid.Utilities.DOM;
     import Settings = Codevoid.Storyvoid.Settings;
 
+    interface HTMLSelectElementWithState extends HTMLSelectElement {
+        isOpen: boolean;
+    }
+
     export class SettingsPopupExperience extends Codevoid.UICore.Control {
         private _handlersToCleanup: Codevoid.Utilities.ICancellable[] = [];
         private viewModel: SettingsPopupViewModel;
         private _navigationManager: Windows.UI.Core.SystemNavigationManager;
-        private _homeArticleLimit: HTMLSelectElement;
-        private _likedArticleLimit: HTMLSelectElement;
-        private _archiveArticleLimit: HTMLSelectElement;
+        private _homeArticleLimit: HTMLSelectElementWithState;
+        private _likedArticleLimit: HTMLSelectElementWithState;
+        private _archiveArticleLimit: HTMLSelectElementWithState;
         private _versionElement: HTMLDivElement;
 
         constructor(element: HTMLElement, options: any) {
@@ -31,8 +35,8 @@
                 this._handlersToCleanup.push(DOM.marryEventsToHandlers(element, this));
                 DOM.marryPartsToControl(element, this);
 
-                this._handlersToCleanup.push(Codevoid.Utilities.addEventListeners(window, {
-                    keyup: (e: KeyboardEvent) => {
+                this._handlersToCleanup.push(Codevoid.Utilities.addEventListeners(this.element, {
+                    keydown: (e: KeyboardEvent) => {
                         switch (e.key.toLowerCase()) {
                             case "esc":
                                 this.close(null);
@@ -40,6 +44,10 @@
                         }
                     }
                 }));
+
+                this._manageOpenStateOnSelectElement(this._homeArticleLimit);
+                this._manageOpenStateOnSelectElement(this._likedArticleLimit);
+                this._manageOpenStateOnSelectElement(this._archiveArticleLimit);
 
                 // Setup OS back button support
                 this._navigationManager.appViewBackButtonVisibility = Windows.UI.Core.AppViewBackButtonVisibility.visible;
@@ -57,8 +65,66 @@
                 this._versionElement.innerText += versionLabel;
 
                 WinJS.Utilities.removeClass(element, "hide");
-                WinJS.UI.Animation.slideUp(this.element);
+                WinJS.UI.Animation.slideUp(this.element).done(() => {
+                    this._homeArticleLimit.focus();
+                });
             });
+        }
+
+        /// <summary>
+        /// This method tries really hard to maintain open state on a select element,
+        /// all with the goal of saying "oh, hey, someone pressed escape. If I'm open
+        /// I should stop that event from proprgating so it doesn't do other things".
+        /// But, if it's not open, then pretent like nothing happened.
+        /// </summary>
+        private _manageOpenStateOnSelectElement(dropDown: HTMLSelectElementWithState): void {
+            this._handlersToCleanup.push(Utilities.addEventListeners(dropDown, {
+                // Keypress is for space, and enter.
+                // Space = open the select. Doesn't do anything when opened
+                // Enter = selects an item in the liste. Doesn't do anything when closed
+                keypress: (e: KeyboardEvent) => {
+                    switch (e.keyCode) {
+                        case WinJS.Utilities.Key.space:
+                            dropDown.isOpen = true;
+                            break;
+
+                        case WinJS.Utilities.Key.enter:
+                            dropDown.isOpen = false;
+                            break;
+
+                    }
+                },
+                // Keydown is to handle the cases where escape (dismiss), or alt+up/down
+                // are pressed. Alt+up/down open/close the dialog, so just toggle the state
+                // Escape is the real key -- it's the handler that stops other elements/handlers
+                // from seeing escape *IF* it's open
+                keydown: (e: KeyboardEvent) => {
+                    switch (e.keyCode) {
+                        case WinJS.Utilities.Key.escape:
+                            console.log("Escaped");
+                            if (dropDown.isOpen) {
+                                e.stopPropagation();
+                            }
+                            dropDown.isOpen = false;
+                            break;
+
+                        case WinJS.Utilities.Key.upArrow:
+                        case WinJS.Utilities.Key.downArrow:
+                            dropDown.isOpen = !dropDown.isOpen;
+                            break;
+                    }
+                },
+                // Click can happen in & out of the dropdown. This basically
+                // handles when you're interacting with it like a normal person.
+                click: (e: KeyboardEvent) => {
+                    dropDown.isOpen = !dropDown.isOpen;
+                },
+                // If you click OUTSIDE the dropdown, theres no event. But
+                // we DO lose focus. assume focus lost means dismissal.
+                focusout: () => {
+                    dropDown.isOpen = false;
+                }
+            }));
         }
 
         public close(args?: Windows.UI.Core.BackRequestedEventArgs): void {
