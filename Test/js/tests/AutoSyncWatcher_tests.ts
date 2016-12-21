2c4a1561a0527d0dd7b1d3395a30beaa27cd6f35
Dominic Hopton <d.hopton@codevoid.net>
2016-12-21 00:16:31 +0000

    Add place holder files, classes, and tests for auto sync watcher
    Additional: cleaned up redudant JS includes in test default.html

---
+++ Test/js/tests/AutoSyncWatcher_tests.ts
@@ -0,0 +1,14 @@
+﻿module CodevoidTests.InstapaperArticleSyncTests {
+    import sv = Codevoid.Storyvoid;
+
+    var promiseTest = InstapaperTestUtilities.promiseTest;
+    var startOnSuccessOfPromise = InstapaperTestUtilities.startOnSuccessOfPromise;
+    var startOnFailureOfPromise = InstapaperTestUtilities.startOnFailureOfPromise;
+
+    QUnit.module("AutoSyncWatcher");
+
+    test("canConstructAutoSyncWatcher", () => {
+        var syncWatcher = new sv.AutoSyncWatcher();
+        ok(syncWatcher, "Couldn't construct sync watcher");
+    });
+}
