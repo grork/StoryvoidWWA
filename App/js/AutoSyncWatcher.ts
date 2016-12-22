61040168e5e391f41664614806db6385c54e0088
Dominic Hopton <d.hopton@codevoid.net>
2016-12-22 06:14:56 +0000

    Support for raising sync needed on entering background / leaving background. Specifically, this is the switch from using Suspend/resume that we *currently* use in the app -- when we swtich to this helper, the change will be effective in there too.
    This also signals if we need a full article sync if we've been backgrounded for long enough.
    
    Includes unit tests, and updates to WinRT.d.ts to support the backgrounding event info.

--- App/js/AutoSyncWatcher.ts
+++ App/js/AutoSyncWatcher.ts
@@ -1,18 +1,25 @@
 ﻿module Codevoid.Storyvoid {
-    export interface IAutoSyncInformation {
+    export interface ISyncNeededEventArgs {
         shouldSyncArticleBodies: boolean;
+        complete(): void;
+    }
+
+    class SyndNeededEventArgs extends Utilities.Signal implements ISyncNeededEventArgs {
+        public shouldSyncArticleBodies: boolean;
     }
 
     export class AutoSyncWatcher {
-        public idleDuration: number = 5 * 1000;
+        public dbIdleDuration = 5 * 1000;
+        public suspendedIdleDuration = 15 * 60 * 1000;
 
         private _eventSource = new Utilities.EventSource();
         private _handlersToCleanup: Utilities.ICancellable[] = [];
         private _currentTimer: WinJS.Promise<any>;
         private _watchingPaused = false;
+        private _suspendedTime = 0;
 
-        constructor(eventSource: Utilities.EventSource) {
-            this._handlersToCleanup.push(Utilities.addEventListeners(eventSource, {
+        constructor(dbEventSource: Utilities.EventSource, appEventSource: Utilities.EventSource) {
+            this._handlersToCleanup.push(Utilities.addEventListeners(dbEventSource, {
                 bookmarkschanged: () => {
                     if (this._watchingPaused) {
                         return;
@@ -21,6 +28,11 @@
                     this._resetTimer();
                 }
             }));
+
+            this._handlersToCleanup.push(Utilities.addEventListeners(appEventSource, {
+                enteredbackground: this._handleEnteringBackground.bind(this),
+                leavingbackground: this._handleLeavingBackground.bind(this),
+            }));
         }
 
         private _resetTimer(): void {
@@ -28,13 +40,18 @@
                 this._currentTimer.cancel();
             }
 
-            this._currentTimer = WinJS.Promise.timeout(this.idleDuration).then(() => {
-                this._considerRaisingSyncNeededEvent();
+            this._currentTimer = WinJS.Promise.timeout(this.dbIdleDuration).then(() => {
+                this._raiseSyncNeeded(false);
             });
         }
 
-        private _considerRaisingSyncNeededEvent(): void {
-            this._eventSource.dispatchEvent("syncneeded", { shouldSyncArticleBodies: false });
+        private _raiseSyncNeeded(syncArticleBodies: boolean): WinJS.Promise<any> {
+            var eventPayload = new SyndNeededEventArgs();
+            eventPayload.shouldSyncArticleBodies = syncArticleBodies;
+
+            this._eventSource.dispatchEvent("syncneeded", eventPayload);
+            
+            return eventPayload.promise;
         }
 
         private _cancelTimer(): void {
@@ -44,6 +61,30 @@
 
             this._currentTimer.cancel();
             this._currentTimer = null;
+        }
+
+        // Note the type here to facilitate unit testing, and I apologise for the
+        // vomit worthy interpretation of the types being f'd up. But, at the time,
+        // I felt this was a better compromise than creating a whole different way
+        // to raise an event just for this one case.
+        private _handleEnteringBackground(e: any): void {
+            var ev: Windows.UI.WebUI.EnteredBackgroundEventArgs = e.detail || e
+
+            this._suspendedTime = Date.now();
+            var deferral = ev.getDeferral();
+
+            this._raiseSyncNeeded(false).done(() => {
+                deferral.complete();
+            }, () => {
+                deferral.complete();
+            });
+        }
+
+        private _handleLeavingBackground() {
+            var timeSuspended = Date.now() - this._suspendedTime;
+            var isLongerThanIdleDuration = (timeSuspended >= this.suspendedIdleDuration);
+
+            this._raiseSyncNeeded(isLongerThanIdleDuration);
         }
 
         public get eventSource(): Utilities.EventSource {
@@ -57,6 +98,20 @@
 
         public resumeWatching(): void {
             this._watchingPaused = false;
+        }
+
+        public dispose(): void {
+            this._cancelTimer();
+
+            if (!this._handlersToCleanup || !this._handlersToCleanup.length) {
+                return;
+            }
+
+            this._handlersToCleanup.forEach((item) => {
+                item.cancel();
+            });
+
+            this._handlersToCleanup = [];
         }
     }
 }
