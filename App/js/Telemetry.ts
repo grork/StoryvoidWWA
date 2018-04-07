7819e37148738efe6add328e77636ffe5bedfcc8
Dominic Hopton <d.hopton@codevoid.net>
2018-04-07 15:56:24 -0700

    Updated Mixpanel Key to be the full app, not test account

--- App/js/Telemetry.ts
+++ App/js/Telemetry.ts
@@ -16,7 +16,7 @@
         private static _client: Codevoid.Utilities.Mixpanel.MixpanelClient;
         static initialize(): WinJS.Promise<any> {
             // Test token
-            Telemetry._client = new Codevoid.Utilities.Mixpanel.MixpanelClient("57368bf65c0e0bd64fec363806164133");
+            Telemetry._client = new Codevoid.Utilities.Mixpanel.MixpanelClient("PLACEHOLDER");
             return Telemetry._client.initializeAsync().then(() => {
                 Telemetry._client.dropEventsForPrivacy = !(new Settings.TelemetrySettings()).telemeteryCollectionEnabled;
                 Telemetry._client.start();
