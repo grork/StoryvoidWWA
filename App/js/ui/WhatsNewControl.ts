1cdaff2538ff5a19a7dbdcb8e5cec095589695f6
Dominic Hopton <d.hopton@codevoid.net>
2018-12-16 13:42:11 -0800

    Add Correct displaying of whats new banner
    Whats new banner now only shows it if it was not previously displayed for some opaque identifier (E.g. if it's different, show it -- no matter how long it was).
    
    Additionally, this behaviour is only triggered if we're automatically signing in (e.g. someone had previously signed in during a previous launch), so we don't show it on first run.
    
    Additional: removed installID 'persistence' since that is now managed by the telemetry library itself.

--- App/js/ui/WhatsNewControl.ts
+++ App/js/ui/WhatsNewControl.ts
@@ -1,9 +1,11 @@
 ﻿module Codevoid.Storyvoid.UI {
     import DOM = Codevoid.Utilities.DOM;
 
+    const WHATS_NEW_RELEASE = "Update2";
+    const WHATS_NEW_MESSAGE = "We've updated! We've added Dark Mode, Picture-in-Picture, Windows Timeline and more.";
+
     export class WhatsNewControl extends Codevoid.UICore.Control {
         private eventSource: Codevoid.Utilities.EventSource;
-        private initialMessage: string;
         private template: WinJS.Binding.Template;
         private _handlersToCleanup: Codevoid.Utilities.ICancellable[] = [];
         private _messageContainer: HTMLElement;
@@ -18,8 +20,7 @@
 
             this.template.render({}, this.element).done(() => {
                 DOM.marryPartsToControl(this.element, this);
-
-                this._messageContainer.textContent = this.initialMessage;
+                this._messageContainer.textContent = WHATS_NEW_MESSAGE
 
                 this._handlersToCleanup.push(Utilities.addEventListeners(this._close, {
                     click: () => this.dismiss()
@@ -41,7 +42,18 @@
 
         public dismiss(): void {
             WinJS.Utilities.addClass(this._close, "hide");
+            WhatsNewControl.markAsShown();
             this.cancelCallback();
+        }
+
+        public static markAsShown(): void {
+            const settings = new Settings.PermanentSettings();
+            settings.whatsNewShownForRelease = WHATS_NEW_RELEASE;
+        }
+
+        public static shouldShowWhatsNew(): boolean {
+            const settings = new Settings.PermanentSettings();
+            return settings.whatsNewShownForRelease != WHATS_NEW_RELEASE;
         }
     }
 }
