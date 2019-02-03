7676d1fb254040b0a434c94542fe4792b7e011a4
Dominic Hopton <d.hopton@codevoid.net>
2019-02-02 16:00:08 -0800

    Removing `stop()` from OAuth tests

--- Test/js/tests/OAuthRequest_tests.js
+++ Test/js/tests/OAuthRequest_tests.js
@@ -11,7 +11,7 @@
                                                              "497571976-w3x216b4KaLDf7si7erTo5CucOvdlzs1QKkFJrfF", /* access token */
                                                              "CM3Kz3VGp5M0UIzycRzrcoBMnAQsPU2K4TE7y3bdk" /* token secret */);
 
-    function authenticationHeaderCorrectlyGenerated() {
+    function authenticationHeaderCorrectlyGenerated(assert) {
         var url = "https://api.twitter.com/1/statuses/update.json";
 
         var data = [{ key: "status", value: "Hello Ladies + Gentlemen, a signed OAuth request!" },
@@ -25,10 +25,10 @@
         var result = request._generateAuthHeader();
         var expectedResult = "oauth_consumer_key=\"xvz1evFS4wEEPTGEFPHBog\", oauth_nonce=\"kYjzVBB8Y0ZFabxSWbWovY3uYSQ2pTgmZeNu2VS4cg\", oauth_signature=\"tnnArxj06cWHq44gCs1OSKk%2FjLY%3D\", oauth_signature_method=\"HMAC-SHA1\", oauth_timestamp=\"1318622958\", oauth_token=\"370773112-GmHxMAgYyLbNEtIKZeRNFsMKPR9EyMZeS9weJAEb\", oauth_version=\"1.0\"";
 
-        QUnit.assert.strictEqual(result, expectedResult, "Encoded header strings didn't match");
+        assert.strictEqual(result, expectedResult, "Encoded header strings didn't match");
     }
 
-    function xAuthAuthenticationHeaderCorrectlyGenerated() {
+    function xAuthAuthenticationHeaderCorrectlyGenerated(assert) {
         var url = "https://api.twitter.com/oauth/access_token";
 
         var clientInfo = new Codevoid.OAuth.ClientInformation("JvyS7DO2qd6NNTsXJ4E7zA",
@@ -46,10 +46,10 @@
         var result = request._generateAuthHeader();
         var expectedResult = "oauth_consumer_key=\"JvyS7DO2qd6NNTsXJ4E7zA\", oauth_nonce=\"6AN2dKRzxyGhmIXUKSmp1JcB4pckM8rD3frKMTmVAo\", oauth_signature=\"1L1oXQmawZAkQ47FHLwcOV%2Bkjwc%3D\", oauth_signature_method=\"HMAC-SHA1\", oauth_timestamp=\"1284565601\", oauth_version=\"1.0\"";
 
-        QUnit.assert.strictEqual(result, expectedResult, "Encoded header strings didn't match");
+        assert.strictEqual(result, expectedResult, "Encoded header strings didn't match");
     }
 
-    function signatureGeneratedCorrectly() {
+    function signatureGeneratedCorrectly(assert) {
         var url = "https://api.twitter.com/1/statuses/update.json";
         var request = new Codevoid.OAuth.OAuthRequest(fakeClientInfo, url);
         request._nonce = "kYjzVBB8Y0ZFabxSWbWovY3uYSQ2pTgmZeNu2VS4cg";
@@ -58,40 +58,40 @@
         var result = request._getSignatureForString("POST&https%3A%2F%2Fapi.twitter.com%2F1%2Fstatuses%2Fupdate.json&include_entities%3Dtrue%26oauth_consumer_key%3Dxvz1evFS4wEEPTGEFPHBog%26oauth_nonce%3DkYjzVBB8Y0ZFabxSWbWovY3uYSQ2pTgmZeNu2VS4cg%26oauth_signature_method%3DHMAC-SHA1%26oauth_timestamp%3D1318622958%26oauth_token%3D370773112-GmHxMAgYyLbNEtIKZeRNFsMKPR9EyMZeS9weJAEb%26oauth_version%3D1.0%26status%3DHello%2520Ladies%2520%252B%2520Gentlemen%252C%2520a%2520signed%2520OAuth%2520request%2521");
         var expectedResult = "tnnArxj06cWHq44gCs1OSKk/jLY=";
 
-        QUnit.assert.strictEqual(result, expectedResult, "Signature wasn't generated correctly");
+        assert.strictEqual(result, expectedResult, "Signature wasn't generated correctly");
     }
 
-    function canVerifyTwitterCredentials() {
+    function canVerifyTwitterCredentials(assert) {
         var url = "https://api.twitter.com/1.1/account/verify_credentials.json";
         var request = new Codevoid.OAuth.OAuthRequest(realClientInfo, url, "GET");
         
-        QUnit.stop();
+        const complete = assert.async();
         request.send().done(function (resultData) {
             var result = JSON.parse(resultData);
-            QUnit.assert.strictEqual(result.screen_name, "CodevoidTest", "couldn't get screen name");
-            QUnit.start();
+            assert.strictEqual(result.screen_name, "CodevoidTest", "couldn't get screen name");
+            complete();
         },
         function (theXhr) {
-            QUnit.assert.ok(false, "request failed");
-            QUnit.start();
+            assert.ok(false, "request failed");
+            complete();
         });
     }
 
-    function canPostStatusToTwitter() {
+    function canPostStatusToTwitter(assert) {
         var url = "https://api.twitter.com/1.1/statuses/update.json";
         var request = new Codevoid.OAuth.OAuthRequest(realClientInfo, url);
 
         request.data = [{ key: "status", value: "Test@Status %78 update: " + Date.now() }];
 
-        QUnit.stop();
+        const complete = assert.async();
         request.send().done(function (resultData) {
             var result = JSON.parse(resultData);
-            QUnit.assert.strictEqual(result.text, request.data[0].value);
-            QUnit.start();
+            assert.strictEqual(result.text, request.data[0].value);
+            complete();
         },
         function (theXhr) {
-            QUnit.assert.ok(false, "request failed");
-            QUnit.start();
+            assert.ok(false, "request failed");
+            complete();
         });
     }
 
@@ -101,54 +101,54 @@
     QUnit.test("signatureGeneratedCorrectly", signatureGeneratedCorrectly);
     QUnit.test("canVerifyTwitterCredentials", canVerifyTwitterCredentials);
     QUnit.test("canPostStatusToTwitter", canPostStatusToTwitter);
-    QUnit.test("canMakeGetRequestWithPayload", function canMakeGetRequestWithPayload() {
+    QUnit.test("canMakeGetRequestWithPayload", function canMakeGetRequestWithPayload(assert) {
         var url = "https://api.twitter.com/1.1/statuses/home_timeline.json";
         var request = new Codevoid.OAuth.OAuthRequest(realClientInfo, url, "GET");
 
         request.data = [{ key: "count", value: 1 }];
 
-        QUnit.stop();
+        const complete = assert.async();
         request.send().done(function (resultData) {
             var result = JSON.parse(resultData);
-            QUnit.assert.strictEqual(Array.isArray(result), true, "Result from query wasn't an array");
-            QUnit.assert.strictEqual(result.length, 1, "Only expected one tweet");
-            QUnit.start();
+            assert.strictEqual(Array.isArray(result), true, "Result from query wasn't an array");
+            assert.strictEqual(result.length, 1, "Only expected one tweet");
+            complete();
         },
         function (theXhr) {
-            QUnit.assert.ok(false, "request failed");
-            QUnit.start();
+            assert.ok(false, "request failed");
+            complete();
         });
     });
 
-    QUnit.test("unreachableHostsFailPredictablyForPostRequest", function unreachableHostsFailPredictablyForPostRequest() {
+    QUnit.test("unreachableHostsFailPredictablyForPostRequest", function unreachableHostsFailPredictablyForPostRequest(assert) {
         var url = "https://a/1.1/statuses/update.json"; // Not real
         var request = new Codevoid.OAuth.OAuthRequest(realClientInfo, url);
 
         request.data = [{ key: "status", value: "Test@Status %78 update: " + Date.now() }];
 
-        QUnit.stop();
+        const complete = assert.async();
         request.send().done(function (resultData) {
-            QUnit.assert.ok(false, "Shouldn't have been successful");
-            QUnit.start();
+            assert.ok(false, "Shouldn't have been successful");
+            complete();
         },
         function (theXhr) {
-            QUnit.assert.ok(true, "Expected request error state");
-            QUnit.start();
+            assert.ok(true, "Expected request error state");
+            complete();
         });
     });
 
-    QUnit.test("unreachableHostsFailPredictablyForGetRequest", function unreachableHostsFailPredictablyForPostRequest() {
+    QUnit.test("unreachableHostsFailPredictablyForGetRequest", function unreachableHostsFailPredictablyForPostRequest(assert) {
         var url = "https://a/1.1/account/verify_credentials.json";
         var request = new Codevoid.OAuth.OAuthRequest(realClientInfo, url, "GET");
 
-        QUnit.stop();
+        const complete = assert.async();
         request.send().done(function (resultData) {
-            QUnit.assert.ok(false, "Didn't expect query to succeed");
-            QUnit.start();
+            assert.ok(false, "Didn't expect query to succeed");
+            complete();
         },
         function (theXhr) {
-            QUnit.assert.ok(true, "Expected request error state");
-            QUnit.start();
+            assert.ok(true, "Expected request error state");
+            complete();
         });
     })
 })();
