4c6fdeb9721d9819e9ed54f4e578236bc64892ee
Dominic Hopton <d.hopton@codevoid.net>
2020-10-10 15:12:43 -0700

    Add Key Template Files + Formats readme.md

---
+++ App_Keys_Template.ts
@@ -0,0 +1,8 @@
+﻿// Fill in the details, and place in %SolutionDir%\App\Keys.ts
+module Codevoid.Storyvoid {
+    // Instapaper Tokens. Replace with tokens & IDs
+    // that are issued to you for Instapapers developer
+    // access: https://www.instapaper.com/main/request_oauth_consumer_token
+    export var INSTAPAPER_CLIENT_ID: string = "PLACEHOLDER";
+    export var INSTAPAPER_CLIENT_SECRET: string = "PLACEHOLDER";
+}
---
+++ Test_Keys_Template.ts
@@ -0,0 +1,29 @@
+// Fill in the details, and place in %SolutionDir%\Test\Keys.ts
+module CodevoidTests {
+    // Twitter Tokens & IDs. Replace with tokens & IDs
+    // that are issued to you from Twitters developer
+    // portal: https://developer.twitter.com/en/docs/developer-portal/overview
+    export var TWITTER_CLIENT_ID: string = "PLACEHOLDER";
+    export var TWITTER_CLIENT_SECRET: string = "PLACEHOLDER";
+    export var TWITTER_ACCESS_TOKEN: string = "PLACEHOLDER";
+    export var TWITTER_ACCESS_TOKEN_SECRET: string = "PLACEHOLDER";
+
+    // Instapaper Tokens. Replace with tokens & IDs
+    // that are issued to you for Instapapers developer
+    // access: https://www.instapaper.com/main/request_oauth_consumer_token
+    export var INSTAPAPER_ACCOUNT: string = "PLACEHOLDER";
+    export var INSTAPAPER_PASSWORD: string = "PLACEHOLDER";
+    export var INSTAPAPER_ACCOUNT_ID: number = -1;
+    export var INSTAPAPER_CLIENT_ID = "PLACEHOLDER";
+    export var INSTAPAPER_CLIENT_SECRET = "PLACEHOLDER";
+    export var INSTAPAPER_TOKEN = "PLACEHOLDER";
+    export var INSTAPAPER_TOKEN_SECRET = "PLACEHOLDER";
+}
+
+module Codevoid.Storyvoid {
+    // Instapaper Tokens. Replace with tokens & IDs
+    // that are issued to you for Instapapers developer
+    // access: https://www.instapaper.com/main/request_oauth_consumer_token
+    export var INSTAPAPER_CLIENT_ID: string = "PLACEHOLDER";
+    export var INSTAPAPER_CLIENT_SECRET: string = "PLACEHOLDER";
+}
